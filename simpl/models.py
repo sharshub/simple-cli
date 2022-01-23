094f79c74796eb9e835d3d6c0026646aadc715b8
Shubham Sharma <sharshub@users.noreply.github.com>
2022-01-23 15:41:57 +0530

    create method for transaction

--- simpl/models.py
+++ simpl/models.py
@@ -1,5 +1,6 @@
 import peewee as orm
 import re
+from decimal import Decimal
 
 from simpl.config import db_name
 
@@ -43,6 +44,12 @@ class User(BaseModel):
     credit_limit = orm.DecimalField(constraints=[orm.Check("credit_limit >= 0")])
     dues = orm.DecimalField(default=0)
 
+    def save(self, **kwargs):
+        assert (
+            self.dues <= self.credit_limit
+        ), "User cannot overspend beyond credit limit"
+        return super().save(**kwargs)
+
     def __str__(self):
         return "{name} ({credit_limit})".format(
             name=self.name, credit_limit=self.credit_limit
@@ -51,12 +58,12 @@ class User(BaseModel):
     @classmethod
     def get_args(self, *args):
         assert (
-            len(args) == 3
+            len(args) >= 3
         ), "Incorrect number of arguments provided, req: user, email, credit_limit"
         _name = args[0]
         _email = args[1]
         try:
-            _credit_limit = float(args[2])
+            _credit_limit = Decimal(args[2])
         except:
             raise Exception("Cannot convert {arg} to a number".format(arg=args[2]))
         return _name, _email, _credit_limit
@@ -93,13 +100,13 @@ class Merchant(BaseModel):
     @classmethod
     def get_args(cls, *args: str):
         assert (
-            len(args) == 2
+            len(args) >= 2
         ), "Incorrect number of arguments provided, req: merchant_name, discount"
         _name = args[0]
         _discount_rate = args[1]
         if not re.match("^[\d\.\,]+\%$", _discount_rate):
-            raise Exception("Incorrect format for discount. Entere discount as: 10%")
-        _discount_rate = float(_discount_rate.replace(",", "").replace("%", ""))
+            raise Exception("Incorrect format for discount. Enter discount as: 10%")
+        _discount_rate = Decimal(_discount_rate.replace(",", "").replace("%", ""))
         return _name, _discount_rate
 
     @classmethod
@@ -123,13 +130,48 @@ class Transaction(BaseModel):
     user = orm.ForeignKeyField(User, backref="transactions")
     merchant = orm.ForeignKeyField(Merchant, backref="transactions")
     amount = orm.DecimalField()
+    merchant_discount = orm.DecimalField()
 
     identifier = "txn"
 
+    def save(self, **kwargs):
+        with db.atomic() as transaction:
+            try:
+                self.user.dues += self.amount
+                self.user.save()
+                self.merchant_discount = (
+                    self.merchant.discount_rate * self.amount / Decimal(100)
+                )
+                return super().save(**kwargs)
+
+            except Exception as e:
+                transaction.rollback()
+                raise e
+
+    @classmethod
+    def get_args(cls, *args: str):
+        assert (
+            len(args) >= 3
+        ), "Incorrect number of arguments provided, req: user, merchant, amount"
+
+        _user_name = args[0]
+        _merchant_name = args[1]
+        _amount = args[2]
+
+        if not re.match("^[\d\.\,]+$", _amount):
+            raise Exception("Incorrect format for amount.")
+
+        _user = User.get(User.name == _user_name)
+        _merchant = Merchant.get(Merchant.name == _merchant_name)
+        _amount = Decimal(_amount.replace(",", ""))
+
+        return _user, _merchant, _amount
+
     @classmethod
     def create_with_args(cls, *args):
-        pass
+        _user, _merchant, _amount = cls.get_args(*args)
+        return cls.create(user=_user, merchant=_merchant, amount=_amount)
 
     @classmethod
     def update_with_args(self, *args):
-        return super().update_with_args(*args)
+        raise Exception("Updating an existing transaction is not allowed")
